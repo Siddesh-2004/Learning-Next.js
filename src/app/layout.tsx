ef93ccf21dd79a3965a62a74e892ae64f2073bed
Siddesh-2004 <siddeshpoojary2004@gmail.com>
2025-08-30 09:25:55 +0530

    added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/sonner"
-
+import NavBar from "@/components/NavBar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <NavBar/>
         {children}
          <Toaster />
       </body>
